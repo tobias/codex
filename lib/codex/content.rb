7d75b65f4974f706b89860f1b67b510822c674c3
tobias <tcrawley@gmail.com>
2009-02-28 23:21:28 -0500

    Broken impl. of repeating slides.

--- lib/codex/content.rb
+++ lib/codex/content.rb
@@ -4,24 +4,44 @@ class Codex::Content
   START_SLIDE = %{<div class="slide">\n}
   END_SLIDE   = %{</div>\n}
   BETWEEN_SLIDES = END_SLIDE + "\n" + START_SLIDE
-
+  HEADING_MARKER = 'h1\. '
+  REPEAT_SLIDE_MARKER = '&::'
+  SPLIT_REGEX = /^(#{HEADING_MARKER}|#{REPEAT_SLIDE_MARKER})/
+  
   def initialize(original)
     @original = original.sub(/__END__.*/m, '').gsub(/__SKIP__.*?__ENDSKIP__/m, '')
   end
 
   def to_html
     textile = Codex::Filters.instance.filter_all(@original)
-    content = split_into_slides(textile)
+    slides = split_into_slides(textile)
+    content = join_slides(slides)
     html = RedCloth.new(content).to_html
   end
   
   def split_into_slides(textile)
-    result = []
-    slides = textile.split(/^h1/).each do |slide|
-      unless slide.empty?
-        result << START_SLIDE << "\nh1" << slide << END_SLIDE
+    delims_and_slides = textile.split(SPLIT_REGEX)
+    #ditch anything before first delim
+    delims_and_slides[1..-1].inject([]) do |accum, s|
+      last_el = accum.pop
+      if last_el.is_a?(String)
+        accum << [last_el, s]
+      else
+        accum << last_el << s
       end
+      accum
+    end
+  end
+
+  def join_slides(slides)
+    result = []
+    slides.each_with_index do |slide_data, index|
+      delim, slide = slide_data
+      result << START_SLIDE << "\nh1. "
+      result << slides[index - 1][1] if delim == REPEAT_SLIDE_MARKER and index > 0
+      result << slide << END_SLIDE
     end
     result.join
   end
+
 end
